a3cf617bd22228fe2be7a11cb168e3dd3998d8cd
NiclasEgeler <70487423+NiclasEgeler@users.noreply.github.com>
2021-12-17 08:24:42 +0100

    remove comments

--- src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/minesweeper/Minesweeper.scala
@@ -5,19 +5,17 @@ import de.htwg.se.minesweeper.builder._
 import de.htwg.se.minesweeper.views.gui._
 
 object Minesweeper {
-  
-  var builder = new ControllerBuilder()
-  var controller = builder.difficulty("easy").random("mock").getResult
-  // var scalafxgui = new GUI()
-  // scalafxgui.run
-  var gui = new SwingGui(controller)
 
-  // Test.start()
-  var tui = new Tui(controller)
-   def main(args: Array[String]) = {
-    println("Welcome to Minesweeper")
-    controller.notifyObservers
-    tui.run()
-    
-  }
+    var builder    = new ControllerBuilder()
+    var controller = builder.difficulty("easy").random("mock").getResult
+
+    var gui = new SwingGui(controller)
+    var tui = new Tui(controller)
+
+    def main(args: Array[String]) = {
+        println("Welcome to Minesweeper")
+        controller.notifyObservers
+        tui.run()
+
+    }
 }
