504b806e3a4a0ddedc69ba63b0bf2e28dfc9f8bc
Pascal-Hagenlocher <pascalihagen2013@gmail.com>
2022-01-19 00:41:36 +0100

    fix tests again

--- src/main/scala/de/htwg/se/minesweeper/controller/modules/HardModule.scala
+++ src/main/scala/de/htwg/se/minesweeper/controller/modules/HardModule.scala
@@ -4,11 +4,12 @@ import de.htwg.se.minesweeper.model.difficulty._
 import de.htwg.se.minesweeper.model.random._
 import de.htwg.se.minesweeper.model.generator._
 import de.htwg.se.minesweeper.controller._
-
+import de.htwg.se.minesweeper.model.fileIO._
 object HardModule {
     given Difficulty = Difficulty.Hard
     given IDifficultyProvider = DifficultyProvider()
     given IRandomProvider = MockRandomProvider()
     given IGenerator = MineGridGenerator()
+    given IFileIO = FileIOToXML()
     given IController = Controller()
 }
